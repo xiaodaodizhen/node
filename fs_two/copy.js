36fe6b8c256823c56eec3b90732456a236cf9000
xiaogu <1296706078@qq.com>
2018-05-10 22:52:36 +0800

    fs

--- fs_two/copy.js
+++ fs_two/copy.js
@@ -18,9 +18,98 @@ let path = require('path');
  * 拷贝的文件有大小限制，兼容存在问题
  * 
  */
-fs.copyFile(path.join(__dirname, '1.txt'), path.join(__dirname, '2.txt'), (err) => {
-  if (err) return console.log(err);
-  console.log('拷贝成功');
-});
+// fs.copyFile(path.join(__dirname, '1.txt'), path.join(__dirname, '2.txt'), (err) => {
+//   if (err) return console.log(err);
+//   console.log('拷贝成功');
+// });
+
+/**
+ * 《读取》
+ * 
+ * 限制读取个数，手动读取
+ * read()
+ * fs.open() 打开文件，先要打开文件，才能对文件进行操作
+ * fd :file descriptor 文件描述符，他代表对当前文件的描述---对应的值是3
+ * 
+ * 
+ * process.stdout.write() :标准输出--1
+ * process.stdout.write() :错误输出--2
+ * 
+ */
+let buffer = Buffer.alloc(3);
+fs.open(path.join(__dirname, '1.txt'), 'r', 0o666, (err, fd) => {
+  /**
+   * 参数：
+   * offset 表示的是buffer从哪个开始存储
+   * length 表示一次想读几个
+   * postion  代表的是文件的读取位置，默认可以写null 代表当前从0开始
+   * length 的长度不能大于buffer的长度
+   * 
+   * bytesRead 读到的个数
+   */
+
+  fs.read(fd, buffer, 0, 3, 0, (err, bytesRead) => {
+    console.log(err, bytesRead);
+  });
+})
 
 
+
+/**
+ * 写入
+ * 
+ */
+
+
+
+
+/**
+ * copy 另一只实现
+ * 
+ */
+
+function copy(source, target) {
+  let size = 3;
+  let buffer = Buffer.alloc(3);
+  fs.open(path.join(__dirname, source), 'r', (err, rfd) => {
+    fs.open(path.join(__dirname, target), 'w', (err, wfd) => {
+      function next() {
+        fs.read(rfd, buffer, 0, size, null, (err, bytesRead) => {
+          if (bytesRead > 0) {// 读取完毕，没有读到就停止
+            fs.write(wfd, buffer, 0, bytesRead, null, (err, bytesWriten) => {
+              // 当write 方法出发了回调函数的时，并不是真正的文件被成功写入，而是先把内容写入到缓存区。
+              next();
+            });
+          } else {
+            // 文件读取完毕后，关闭。
+            fs.close(rfd, () => {
+              console.log('读取成功');
+            });
+
+            // 当读取完毕后，就强制将内容从缓存区写入文件。
+            fs.fsync(wfd, () => {
+              // 文件写入完毕后，关闭。
+              fs.close(wfd, () => {
+                console.log('关闭', '拷贝成功');
+              });
+            });
+          }
+        })
+      }
+      next();
+    })
+  });
+}
+
+copy('1.txt', '2.txt');
+
+
+// read()  
+
+/**
+ * readFile():读取文件的全部内容
+ * writeFile() :将内容全部写入文件
+ * fs.read(fd, buffer, offset, length, position, callback):从fd指定的文件中读取数据到buffer(缓存区)
+ *fs.write(fd, buffer[, offset[, length[, position]]], callback):将buffer中的内容写入到fd指定的文件中去
+ * 
+ */
---
+++ npm/1npm.js
@@ -0,0 +1,25 @@
+// 第三方模块（我们下载的模块），不需要加./引得是module.paths
+//安装前npm init -y ，初始化。
+// 初始化一个package.json 不能写注释，不能加标点
+//如果没有初始化，记录依赖的功能就没了。
+
+//开发依赖：webpack  babel 
+//项目依赖：react redux
+
+
+// npm install 包名字  --save-dev  安装开发依赖
+// npm uninstall 包名字  --save-dev   卸载项目依赖
+
+// npm 配置文件，可以更改下载的来晕啊，和下载到什么位置，代理配置； npm config list   npm config ls -l
+ 
+
+
+
+//yarn
+//全局命令，在命令行下使用，
+// npm root -g 查看全局的包安装地址
+// npm install -g yarn
+// yarn init -y
+// yarn add 文件名@版本号 --dev
+// yarn remove 文件名
+// yarn install 
