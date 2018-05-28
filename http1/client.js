93bc03b1b5948f37b8aaf9b71044f7071df81552
xiaogu <1296706078@qq.com>
2018-05-28 22:34:07 +0800

    http

--- http1/client.js
+++ http1/client.js
@@ -13,8 +13,17 @@ let options = {
 
     }
 };
-let req = http.request(options);
+let req = http.request(options, (res) => {
+    // 请求发送成功后会调用此函数，拿到服务端返回的结果
+    res.on('data', (data) => {
+        console.log(data);
+    });
 
+});
 // 前后端通信靠的都是json字符串
+// 向服务端发消息 ---方法一
+// req.write('{"name":"zfed"}');
+
+// 向服务端发消息（带有结束功能） ---方法二
 req.end('name=zfpx&&age=9');// 表单提交式内容传送方式
-// req.end('{"name":"zfed"}');// 客户端的请求发送完毕
+// req.end('{"name":"zfed"}');// 客户端的请求发送完毕,发送的内容可以为空
--- http1/req.js
+++ http1/req.js
@@ -6,12 +6,12 @@ let server = http.createServer((req, res) => {
     // 响应可以设置响应头
 
     // 默认情况下返回给客户端内容 （状态码）200
-    // 方法一
+    // 方法一   可以使用多次
     // res.statusCode = 200;
     // res.setHeader('Content-Type', 'text/plain');
     // res.setHeader('name', 'zfpex');
 
-    // 方法二
+    // 方法二  只可以设置一次
     res.writeHead(200, { 'Content-Type': 'text/plain' });
 
     // 取响应头，方法一不会真正的把响应头写给客户端，方法二可以，,通过res.headersSent来进行查看，返回布尔, 备注：方法一返回false，方法二返回true；；res.setHeader与res.writeHead不能同时使用。
